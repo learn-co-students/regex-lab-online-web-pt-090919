b7fa8e5b2e984af05ac2d6bc54091f57ead02927
Emily Harber <emily.a.harber@gmail.com>
2019-09-20 03:52:58 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+def starts_with_a_vowel?(word)
+ if word.scan(/\b[aeiou, AEIOU]/).count > 0
+   true 
+ else 
+   false 
+ end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b(un\w*ing)/).flatten
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/).flatten 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.scan(/\b[A-Z]\w*[.,?!]/).count > 0
+    true
+  else 
+    false 
+  end 
 end
 
 def valid_phone_number?(phone)
-
+  if phone.scan(/.?(\d{3}).?(\d{3}).?(\d{4})/).count > 0
+    true 
+  else 
+    false 
+  end 
 end
+
