1e8851b73b5a1f1abbf19f3d90bb66e31b40dbb8
Brianna Jordan <bryvjordan9@gmail.com>
2019-09-25 22:15:25 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,21 @@
 def starts_with_a_vowel?(word)
 
 end
+end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
+end
 end
 
 def words_five_letters_long(text)
 
 end
+end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
+end
 end
 
 def valid_phone_number?(phone)
